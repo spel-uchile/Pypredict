3c4c367df8b02e5a9fc25617b57fe2703f62734c
matvidal <matias.vidal.v@gmail.com>
2019-01-24 19:32:39 +0900

    Added the installation and upgrade of pip.

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ sudo apt-get install python3-scipy
 sudo apt-get install python3-pip
 sudo apt-get install libproj-dev
 sudo apt-get install libgeos-dev
+sudo apt-get install python3-pip
+pip install --upgrade pip
 sudo pip3 install cython
 sudo pip3 install cartopy
