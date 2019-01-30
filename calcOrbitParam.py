989c78f4dd6e858a0889e043da0d07e957fc7e1c
matvidal <matias.vidal.v@gmail.com>
2019-01-30 17:48:35 +0900

    Imported from numpy only the used functions. Changed RAN to RAAN whenever possible. Now the object receives a list [x, y, z] instead of a matrix.

--- calcOrbitParam.py
+++ calcOrbitParam.py
@@ -1,40 +1,45 @@
-import numpy as np
+from numpy import abs, arccos, arctan, argmin, cos, cross, dot, pi, sin, sqrt, tan, zeros 
 
 class Calc():
 
-    def __init__(self, dt, r1, r2, Mt=5.9722*10**24):
-        self.newCalc(dt, r1, r2)
-        self.getVelocities()
-        self.getOrbitalParameters(self.r1, self.v1)
+    def __init__(self, r1, dt=None, r2=None, v=None, Mt=5.9722*10**24):
+        self.G = 6.67408*10**(-11)
+        self.newCalc(r1, dt=dt, r2=r2, v=v, Mt=Mt)
     
     def __call__(self):
         return self
     
-    def newCalc(self, dt, r1, r2, Mt=5.9722*10**24):
-        G = 6.67408*10**(-11)
-        self.dt = dt
+    def newCalc(self, r1, dt=None, r2=None, v=None, Mt=5.9722*10**24):
         self.r1 = r1
-        self.r2 = r2
-        self.mu = G*Mt
+        self.mu = self.G*Mt
+        if (dt is not None and r2 is not None):
+            self.dt = dt
+            self.r2 = r2
+            self.getVelocities()
+        elif (v is not None):
+            self.v1 = v
+        else:
+            print("Two positions and time or a position and a velocity are needed.")
+        self.getOrbitalParameters(self.r1, self.v1)
         
     def getdtheta(self):
-        r1xr2 = np.cross(self.r1, self.r2)
-        if (r1xr2[0, 2] >= 0):
-            dtheta = np.arccos(np.dot(self.r1, self.r2.transpose())/(self.r1_scalar*self.r2_scalar))
-        elif (r1xr2[0, 2] < 0):
-            dtheta = 2*np.pi - np.arccos(np.dot(self.r1, self.r2.transpose())/(self.r1_scalar*self.r2_scalar))
-        return np.asscalar(dtheta)
+        r1xr2 = cross(self.r1, self.r2)
+        if (r1xr2[2] >= 0):
+            dtheta = arccos(dot(self.r1, self.r2)/(self.r1_scalar*self.r2_scalar))
+        elif (r1xr2[2] < 0):
+            dtheta = 2*pi - arccos(dot(self.r1, self.r2)/(self.r1_scalar*self.r2_scalar))
+        return dtheta
         
     def getA(self, dtheta):
-        A = np.sin(dtheta)*np.sqrt(self.r1_scalar*self.r2_scalar/(1 - np.cos(dtheta)))
+        A = sin(dtheta)*sqrt(self.r1_scalar*self.r2_scalar/(1 - cos(dtheta)))
         return A
         
     def gety(self, A, C, S, z):
-        y = self.r1_scalar + self.r2_scalar + A*(z*S - 1)/np.sqrt(C)
+        y = self.r1_scalar + self.r2_scalar + A*(z*S - 1)/sqrt(C)
         return y
         
     def getyp(self, A, c):
-        yp = A/4*np.sqrt(C)
+        yp = A/4*sqrt(C)
         return yp
         
     def getf(self, y):   
@@ -42,7 +47,7 @@ class Calc():
         return f
         
     def getg(self, A, y):
-        g = A*np.sqrt(y/self.mu)
+        g = A*sqrt(y/self.mu)
         return g
         
     def getgp(self, y):
@@ -50,14 +55,14 @@ class Calc():
         return gp
         
     def getF(self, A, C, S, y):
-        F = (y/C)**(3/2)*S + A*np.sqrt(y) - np.sqrt(self.mu)*self.dt
+        F = (y/C)**(3/2)*S + A*sqrt(y) - sqrt(self.mu)*self.dt
         return F
         
     def getFp(self, A, C, S, y, z):
         if (z == 0):
-            Fp = np.sqrt(2)/40*y**(3/2) + A/8*(np.sqrt(y) + A*np.sqrt(1/(2*y)))
+            Fp = sqrt(2)/40*y**(3/2) + A/8*(sqrt(y) + A*sqrt(1/(2*y)))
         else:
-            Fp = (y/C)**(3/2)*(1/(2*z)*(C - 3*S/(2*C)) + 3/4*S**2/C) + A/8*(3*S/C*np.sqrt(y) + A*np.sqrt(C/y))
+            Fp = (y/C)**(3/2)*(1/(2*z)*(C - 3*S/(2*C)) + 3/4*S**2/C) + A/8*(3*S/C*sqrt(y) + A*sqrt(C/y))
         return Fp
         
     def getz(self, z0, F, Fp):
@@ -67,7 +72,7 @@ class Calc():
     def getzNewton(self, A, z0):
         z = z0
         z0 = 0
-        while (np.abs(z0 - z) > 0.00001):
+        while (abs(z0 - z) > 0.00001):
             z0 = z
             C = 1/2 - z0/24
             S = 1/6 - z0/120
@@ -78,20 +83,19 @@ class Calc():
         return z
         
     def getz0(self, A):
-        F_array = np.zeros(30)
-        z_array = np.zeros(30)
+        F_array = zeros(30)
+        z_array = zeros(30)
         for i in range(0, 30, 1):
             z_array[i] = i/10
             C = 1/2 - z_array[i]/24
             S = 1/6 - z_array[i]/120
             y = self.gety(A, C, S, z_array[i])
             F_array[i] = self.getF(A, C, S, y)
-        z0 = z_array[np.argmin(np.abs(F_array))]
+        z0 = z_array[argmin(abs(F_array))]
         return z0
         
     def getMagnitude(self, vect):
-        scalar = np.asscalar(np.sqrt(np.dot(vect, vect.transpose())))
-        return scalar
+        return sqrt(dot(vect, vect))
         
     def getVelocities(self):
         self.r1_scalar = self.getMagnitude(self.r1)
@@ -105,63 +109,111 @@ class Calc():
         y = self.gety(A, C, S, z)
         f = self.getf(y)
         g = self.getg(A, y)
-        gp = self.getgp(y)  
-        self.v1 = 1/g*(self.r2 - f*self.r1)
-        self.v2 = 1/g*(gp*self.r2 - self.r1)
+        gp = self.getgp(y)
+        self.v1 = [0, 0, 0]
+        self.v2 = [0, 0, 0]
+        for i in range(3):
+            self.v1[i] = 1/g*(self.r2[i] - f*self.r1[i])
+            self.v2[i] = 1/g*(gp*self.r2[i] - self.r1[i])
         
     def getRadialVel(self, r, r_scalar, v):
-        vr = np.asscalar(np.dot(r, v.transpose()))/r_scalar
-        return vr
+        self.vr = dot(r, v)/r_scalar
         
     def getInclination(self, h):
-        self.i = np.arccos(h[0, 2]/self.h_scalar)
+        self.i = arccos(h[2]/self.h_scalar)
         
-    def getRAN(self, N, N_scalar):
-        if (N[0, 1] >= 0):
-            self.RAN = np.arccos(N[0, 0]/N_scalar)
+    def getRAAN(self, N, N_scalar):
+        if (N[1] >= 0):
+            self.RAAN = arccos(N[0]/N_scalar) % (2*pi)
         else:
-            self.RAN = 2*np.pi - np.arccos(N[0, 0]/N_scalar)
+            self.RAAN = (2*pi - arccos(N[0]/N_scalar)) % (2*pi)
         
-    def getExcentricity(self, r, v, vr):
+    def getExcentricity(self, r, v):
         r_scalar = self.getMagnitude(r)
         v_scalar = self.getMagnitude(v)
-        e = 1/self.mu*((v_scalar**2 - self.mu/r_scalar)*r - r_scalar*vr*v)
-        return e
+        self.e = [0, 0, 0]
+        for i in range(3):
+            self.e[i] = 1/self.mu*((v_scalar**2 - self.mu/r_scalar)*r[i] - r_scalar*self.vr*v[i])
         
-    def getArgOfPerigee(self, N, N_scalar, e):
-        arg = 1/(N_scalar*self.e_scalar)*np.asscalar(np.dot(N, e.transpose()))
-        if (e[0,2] >= 0):
-            self.w = np.arccos(arg)
+    def getArgOfPerigee(self, N, N_scalar):
+        arg = 1/(N_scalar*self.e_scalar)*dot(N, self.e)
+        if (self.e[2] >= 0):
+            self.w = arccos(arg)
         else:
-            self.w = 2*np.pi - np.arccos(arg)
+            self.w = 2*pi - arccos(arg)
         
-    def getTrueAnomaly(self, e, r, r_scalar, vr):
-        arg = 1/(self.e_scalar*r_scalar)*np.asscalar(np.dot(e, r.transpose()))
-        if (vr >= 0):
-            self.theta = np.arccos(arg)
+    def getTrueAnomaly(self, r, r_scalar):
+        arg = 1/(self.e_scalar*r_scalar)*dot(self.e, r)
+        if (self.vr >= 0):
+            self.theta = arccos(arg)
         else:
-            self.theta = 2*np.pi - np.arccos(arg)
+            self.theta = 2*pi - arccos(arg)
+
+    def theta2E(self):
+        return 2*tan(sqrt((1 - self.e_scalar)/(1 + self.e_scalar))*tan(self.theta/2))
+
+    def getMeanAnomaly(self):
+        E = self.theta2E()
+        self.MA = E - self.e_scalar*sin(E)
         
     def getSemimajorAxis(self):
-        rp = self.h_scalar**2/self.mu*(1/(1 + self.e_scalar*np.cos(0)))
-        ra = self.h_scalar**2/self.mu*(1/(1 + self.e_scalar*np.cos(np.pi)))
+        rp = self.h_scalar**2/self.mu*(1/(1 + self.e_scalar*cos(0)))
+        ra = self.h_scalar**2/self.mu*(1/(1 + self.e_scalar*cos(pi)))
         self.a = 0.5*(rp + ra)
-        
+
+    def getMeanVelocity(self):
+        self.n = sqrt(self.mu/self.a**3)
+
     def getPeriod(self):
-        self.T = 2*np.pi/np.sqrt(self.mu)*self.a**(3/2)
+        self.T = 2*pi/self.n
+
+    def getMeanMotion(self):
+        self.MM = self.n*12*3600/pi
         
     def getOrbitalParameters(self, r, v):
         r_scalar = self.getMagnitude(r)
-        h = np.cross(r, v)
-        self.h_scalar = self.getMagnitude(h)
-        vr = self.getRadialVel(r, r_scalar, v)
-        e = self.getExcentricity(r, v, vr)
-        self.e_scalar = self.getMagnitude(e)
+        h = cross(r, v)
+        self.h_scalar = self.getMagnitude(h/1000000)*1000000
+        self.getRadialVel(r, r_scalar, v)
+        self.getExcentricity(r, v)
+        self.e_scalar = self.getMagnitude(self.e)
         self.getSemimajorAxis()
-        N = np.cross([[0, 0, 1]], h)
-        N_scalar = self.getMagnitude(N)
-        self.getRAN(N, N_scalar)
+        N = cross([0, 0, 1], h)
+        N_scalar = self.getMagnitude(N/1000000)*1000000
+        self.getRAAN(N, N_scalar)
         self.getInclination(h)
-        self.getArgOfPerigee(N, N_scalar, e)
-        self.getTrueAnomaly(e, r, r_scalar, vr)
+        self.getArgOfPerigee(N, N_scalar)
+        self.getTrueAnomaly(r, r_scalar)
+        self.getMeanAnomaly()
+        self.getMeanVelocity()
+        self.getMeanMotion()
         self.getPeriod()
+
+
+    def getTLE(self):
+        self.TLE1 = "{} {}{} {}{}{} {}{} {} {} {} {} {}{}".format("1",
+                                                                  "     ",
+                                                                  "U",
+                                                                  "  ",
+                                                                  "   ",
+                                                                  "   ",
+                                                                  "  ",
+                                                                  "            ",
+                                                                  "          ",
+                                                                  "        ",
+                                                                  "        ",
+                                                                  "        ",
+                                                                  "0",
+                                                                  "    ",
+                                                                  " ")
+        self.TLE2 = "{} {} {:08.4f} {:08.4f} {:07d} {:08.4f} {:08.4f} {:011d}{}{}".format("2",
+                                                         "     ",
+                                                         self.i*180/pi,
+                                                         self.RAAN*180/pi,
+                                                         self.e*10000000,
+                                                         self.w*180/pi,
+                                                         self.MA*180/pi,
+                                                         self.MM,
+                                                         "     ",
+                                                         " ")
+        return "{}\n{}".format(self.TLE1, self.TLE2)
