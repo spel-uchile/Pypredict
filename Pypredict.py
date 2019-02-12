ad6834437f16b4ba667429b101031043983f1453
matvidal <matias.vidal.v@gmail.com>
2019-02-12 22:11:15 +0900

    Deleted the update instruction of the TLEs for the sample satellites, now the data will be taken from local files for the initialization of Pypredict. Added instructions to calculate all the orbital parameters of a Femto-satellite deployed from SUCHAI at current time. The femtosatellite FE1 will be displayed along with other satellites from the begining.

--- Pypredict.py
+++ Pypredict.py
@@ -4,31 +4,31 @@ from sat import Sat
 from tdoa import TDOA
 from gui import GUI
 
-#tlefile.TLE_URLS = ("https://celestrak.com/NORAD/elements/cubesat.txt", )
-#tlefile.fetch("TLE/cubesat.txt")
-#tlefile.TLE_URLS = ('https://celestrak.com/NORAD/elements/iridium.txt', )
-#tlefile.fetch("TLE/iridium.txt")
-tlefile.TLE_URLS =  ("https://celestrak.com/NORAD/elements/argos.txt",
-                     "https://celestrak.com/NORAD/elements/cubesat.txt",
-                     "https://celestrak.com/NORAD/elements/dmc.txt",
-                     "https://celestrak.com/NORAD/elements/dmc.txt",
-                     "https://celestrak.com/NORAD/elements/intelsat.txt",
-                     "https://celestrak.com/NORAD/elements/iridium.txt",
-                     "https://celestrak.com/NORAD/elements/iridium-NEXT.txt",
-                     "https://celestrak.com/NORAD/elements/noaa.txt",
-                     "https://celestrak.com/NORAD/elements/planet.txt",
-                     "https://celestrak.com/NORAD/elements/resource.txt",
-                     "https://celestrak.com/NORAD/elements/sarsat.txt",
-                     "https://celestrak.com/NORAD/elements/spire.txt",
-                     "https://celestrak.com/NORAD/elements/tdrss.txt",
-                     "https://celestrak.com/NORAD/elements/visual.txt",
-                     "https://celestrak.com/NORAD/elements/weather.txt", )
 SUCHAI = Sat(name="SUCHAI", tle=tlefile.read("SUCHAI", "TLE/cubesat.txt"), cat="CubeSat")
-IRIDIUM90 = Sat(name="IRIDIUM 90", tle=tlefile.read("IRIDIUM 90 [-]", "TLE/iridium.txt"), cat="Iridium")
-IRIDIUM91 = Sat(name="IRIDIUM 91", tle=tlefile.read("IRIDIUM 91 [+]", "TLE/iridium.txt"), cat="Iridium")
-IRIDIUM95 = Sat(name="IRIDIUM 95", tle=tlefile.read("IRIDIUM 95 [+]", "TLE/iridium.txt"), cat="Iridium")
+HODOYOSHI3 = Sat(name="HODOYOSHI-3", tle=tlefile.read("HODOYOSHI-3", "TLE/resource.txt"), cat="Earth Resources")
+HODOYOSHI4 = Sat(name="HODOYOSHI-4", tle=tlefile.read("HODOYOSHI-4", "TLE/resource.txt"), cat="Earth Resources")
 #tdoa = TDOA()
-Sats = [SUCHAI, IRIDIUM90, IRIDIUM91, IRIDIUM95]
+[x, y, z] = SUCHAI.getXYZ()
+r = [x, y, z]
+[v_x, v_y, v_z] = SUCHAI.getVelocityVector()
+v = [v_x, v_y, v_z]
+calc = Calc(r, v=v)
+FE1 = Sat(name="FE1", tle=tlefile.read("SUCHAI", "TLE/cubesat.txt"), cat="Femto-satellite")
+FE1.updateEpoch()
+FE1.updateGST0()
+FE1.setInclination(calc.i)
+FE1.setRAAN0(calc.RAAN)
+FE1.setArgPerigee0(calc.w)
+FE1.setEccentricity(calc.e_scalar)
+FE1.setMeanAnomaly0(calc.MA)
+FE1.setTrueAnomaly(calc.theta)
+FE1.setSemiMajorAxis(calc.a)
+FE1.setSemilatusRectum(calc.a*(1 - calc.e_scalar**2))
+FE1.setMeanVelocity(calc.n)
+FE1.p = calc.a*(1 - calc.e_scalar**2)
+FE1.updateOrbitalParameters()
+
+Sats = [SUCHAI, HODOYOSHI3, HODOYOSHI4, FE1]
 #x, y, z = tdoa.calculateLocation(SUCHAI, IRIDIUM83, IRIDIUM90, IRIDIUM91, IRIDIUM95)
 #print("NOI real x: " + str(IRIDIUM83.x) + "    NOI calculated x: " + str(x))
 #print("NOI real y: " + str(IRIDIUM83.y) + "    NOI calcualted y: " + str(y))
