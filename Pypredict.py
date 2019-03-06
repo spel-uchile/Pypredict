b8411e06e4cff88226cce30cce561b164b3cd385
matvidal <matias.vidal.v@gmail.com>
2019-03-06 20:29:40 +0900

    Replaced Microdragon with CubeSat XI-VI.

--- Pypredict.py
+++ Pypredict.py
@@ -6,10 +6,10 @@ from gui import GUI
 SUCHAI = Sat(name="SUCHAI", tle=tlefile.read("SUCHAI", "TLE/cubesat.txt"), cat="CubeSat")
 HODOYOSHI3 = Sat(name="HODOYOSHI-3", tle=tlefile.read("HODOYOSHI-3", "TLE/resource.txt"), cat="Earth Resources")
 HODOYOSHI4 = Sat(name="HODOYOSHI-4", tle=tlefile.read("HODOYOSHI-4", "TLE/resource.txt"), cat="Earth Resources")
-MICRODRAGON = Sat(name="MICRODRAGON", tle=tlefile.read("MICRODRAGON", "TLE/tle-new.txt"), cat="Last 30 Days' Launches")
+CUBESATXI_IV = Sat(name="CUBESAT XI-IV (CO-57)", tle=tlefile.read("CUBESAT XI-IV (CO-57)", "TLE/cubesat.txt"), cat="CubeSat")
 #tdoa = TDOA()
 
-Sats = [SUCHAI, HODOYOSHI3, HODOYOSHI4, MICRODRAGON]
+Sats = [SUCHAI, HODOYOSHI3, HODOYOSHI4, CUBESATXI_IV]
 #x, y, z = tdoa.calculateLocation(SUCHAI, IRIDIUM83, IRIDIUM90, IRIDIUM91, IRIDIUM95)
 #print("NOI real x: " + str(IRIDIUM83.x) + "    NOI calculated x: " + str(x))
 #print("NOI real y: " + str(IRIDIUM83.y) + "    NOI calcualted y: " + str(y))
