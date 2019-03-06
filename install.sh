f3637436564c7db382b62b293517f9c50777ce53
matvidal <matias.vidal.v@gmail.com>
2019-03-06 21:15:12 +0900

    Now the install file upgrades the version of cython and cartopy. If you want to use matplotlib 3.x.x, use the commented lines of this install file.

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ sudo apt-get install libproj-dev
 sudo apt-get install libgeos-dev
 sudo apt-get install python3-pip
 pip install --upgrade pip
-sudo pip3 install cython
-sudo pip3 install cartopy
+#sudo pip3 install --upgrade matplotlib
+sudo pip3 install --upgrade cython
+sudo pip3 install --upgrade cartopy
+#sudo apt-get install libtcmalloc-minimal4
+#export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4.3.0"
+
