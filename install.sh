8555de077c8cf33743e5f26f7491733389df8a8d
matvidal <matias.vidal.v@gmail.com>
2019-03-07 15:35:40 +0900

    Added the explicit installation of the dependencies to install cartopy.

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
+sudo apt-get install python3-dev
 sudo apt-get install python3-pyorbital
 sudo apt-get install python3-matplotlib
 sudo apt-get install python3-scipy
 sudo apt-get install python3-pip
+sudo apt-get install libjs-proj4
 sudo apt-get install libproj-dev
-sudo apt-get install libgeos-dev
+#sudo apt-get install libgeos-dev
 sudo apt-get install python3-pip
 sudo -H pip3 install --upgrade pip
 #sudo pip3 install --upgrade matplotlib
 sudo -H pip3 install --upgrade cython
 sudo -H pip3 install --upgrade numpy
+sudo -H pip3 install --upgrade geos
+sudo -H pip3 install --upgrade shapely
+sudo -H pip3 install --upgrade pyshp
+sudo -H pip3 install --upgrade six
+sudo -H pip3 install --upgrade Pillow
+sudo -H pip3 install --upgrade Fiona
+sudo -H pip3 install --upgrade pykdtree
 sudo -H pip3 install --upgrade --no-use-pep517 cartopy
 #sudo apt-get install libtcmalloc-minimal4
 #export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4.3.0"
