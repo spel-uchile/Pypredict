d9b9174de285c44984612f08ee76118ada7effc2
matvidal <matias.vidal.v@gmail.com>
2019-03-07 12:27:09 +0900

    Added the --no-use-pep517 flag for the cartopy installation.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt-get install python3-pip
 sudo -H pip3 install --upgrade pip
 #sudo pip3 install --upgrade matplotlib
 sudo -H pip3 install --upgrade cython
-sudo -H pip3 install --upgrade cartopy
+sudo -H pip3 install --upgrade --no-use-pep517 cartopy
 #sudo apt-get install libtcmalloc-minimal4
 #export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4.3.0"
 
