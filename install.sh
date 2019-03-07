78b40e51cf026416a6daf7d5a5de692919071d8a
matvidal <matias.vidal.v@gmail.com>
2019-03-07 11:51:04 +0900

    Fixed an issue in which the pip2 environment was upgraded instead of pip3. Also now the installation with pip is using the -H flag.

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo apt-get install python3-pip
 sudo apt-get install libproj-dev
 sudo apt-get install libgeos-dev
 sudo apt-get install python3-pip
-pip install --upgrade pip
+sudo -H pip3 install --upgrade pip
 #sudo pip3 install --upgrade matplotlib
-sudo pip3 install --upgrade cython
-sudo pip3 install --upgrade cartopy
+sudo -H pip3 install --upgrade cython
+sudo -H pip3 install --upgrade cartopy
 #sudo apt-get install libtcmalloc-minimal4
 #export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4.3.0"
 
