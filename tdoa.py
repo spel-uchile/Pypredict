8041e9b0094f1068c42c78bccbd43a68a37f7d3d
matvidal <matias.vidal.v@gmail.com>
2019-03-12 16:10:57 +0900

    Fixed an error in the equations. The asscalar function is no longer needed. This class only supports 3D localization.

--- tdoa.py
+++ tdoa.py
@@ -1,4 +1,4 @@
-from numpy import asscalar, matrix, sqrt
+from numpy import matrix, sqrt
 
 class TDOA(object):
     __slots__ = ["c"]
@@ -9,6 +9,10 @@ class TDOA(object):
         return self
 
     def calculateLocation(self, r0, r1, r2, r3, r4=None):
+        r0 = [r0[0,0], r0[1,0], r0[2,0]]
+        r1 = [r1[0,0], r1[1,0], r1[2,0]]
+        r2 = [r2[0,0], r2[1,0], r2[2,0]]
+        r3 = [r3[0,0], r3[1,0], r3[2,0]]
         x1 = r1[0]
         y1 = r1[1]
         x21 = r2[0] - x1
@@ -19,32 +23,9 @@ class TDOA(object):
         d31 = self.c*self.getdt(r0, r1, r3)
         if (r4 is None):
             print("This result is not valid for 3D coordinates, one more satellite is needed")
-            C1 = x1**2 + y1**2
-            C2 = r2[0]**2 + r2[1]**2
-            C3 = r3[0]**2 + r3[1]**2
-            a = ((y21*d31 - y31*d21)**2 + (x31*d21 - x21*d31)**2 - (x31*y21 - x21*y31)**2)
-            b = 2*(y21*d31 - y31*d21)*(x1*(x31*y21 - x21*y31)**2 + y21/2*(d31**2 - C3 + C1) - y31/2*(d21**2 - C2 + C1))
-            b = b + 2*(x31*d21 - x21*d31)*(y1*(x31*y21 - x21*y31)**2 + x31/2*(d21**2 - C2 + C1) - x21/2*(d31**2 - C3 + C1))
-            c = (x31/2*(d21**2 - C2 + C1) - x21/2*(d31**2 - C3 + C1))**2 + (y21/2*(d31**2 - C3 + C1) - y31/2*(d21**2 - C2 + C1))**2
-            c = c + C1*(x31*y21 - x21*y31)**4
-            c = c + 2*x1*(x31*y21 - x21*y31)**2*(y21/2*(d31**2 - C3 + C1) - y31/2*(d21**2 - C2 + C1))
-            c = c + 2*y1*(x31*y21 - x21*y31)**2*(x31/2*(d21**2 - C2 + C1) - x21/2*(d31**2 - C3 + C1))
-            print("a: " + str(a))
-            print("b: " + str(b))
-            print("c: " + str(c))
-            d1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
-            print("d1 real: " + str(self.getDistance(r0, r1)) + "    d1 calculated: " + str(d1))
-            #M = matrix(((x21, y21),
-            #               (x31, y31)))
-            #H = self.getH(r0, r1, r2, r3)
-            #x, y = -M.I*(matrix(((d21), (d31))).transpose()*d1 + H)
-            x = -y21/(x31*y21 - x21*y31)*(d31*d1 + 1/2*(d31**2 - C3 + C1))
-            x = x + y31/(x31*y21 - x21*y31)*(d21*d1 + 1/2*(d21**2 - C2 + C1))
-            y = -x31/(x31*y21 - x21*y31)*(d21*d1 + 1/2*(d21**2 - C2 + C1))
-            y = y + x21/(x31*y21 - x21*y31)*(d31*d1 + 1/2*(d31**2 - C3 + C1))
-            z = r1[2]
+            pass
         else:
-            print("This result is valid for 3D coordinates")
+            r4 = [r4[0,0], r4[1,0], r4[2,0]]
             z1 = r1[2]
             C1 = self.getC(r1)
             C2 = self.getC(r2)
@@ -62,7 +43,7 @@ class TDOA(object):
             a3 = (x31*y41 - x41*y31)*d21 - (x21*y41 - x41*y21)*d31 - (x31*y21 - x21*y31)*d41
             a = a1**2 + a2**2 + a3**2 - D
             b = a1*(2*x1*D + (y31*z41 - y41*z31)*(d21**2 - C2 + C1) - (y21*z41 - y41*z21)*(d31**2 - C3 + C1) - (y31*z21 - y21*z31)*(d41**2 - C4 + C1))
-            b += a2*(2*y1*D - (x31*z41 - x41*z31)*(d21**2 - C2 + C1) + (x21*z41 - x41*z21)*(d31**2 - C3 + C1) - (x21*z31 - x31*z21)*(d41**2 - C4 + C1))
+            b += -a2*(2*y1*D - (x31*z41 - x41*z31)*(d21**2 - C2 + C1) + (x21*z41 - x41*z21)*(d31**2 - C3 + C1) - (x21*z31 - x31*z21)*(d41**2 - C4 + C1))
             b += a3*(2*z1*D + (x31*y41 - x41*y31)*(d21**2 - C2 + C1) - (x21*y41 - x41*y21)*(d31**2 - C3 + C1) - (x31*y21 - x21*y31)*(d41**2 - C4 + C1))
             c = x1**2*D**2 + 1/4*(y31*z41 - y41*z31)**2*(d21**2 - C2 + C1)**2 + 1/4*(y21*z41 - y41*z21)**2*(d31**2 - C3 + C1)**2
             c += 1/4*(y31*z21 - y21*z31)**2*(d41**2 - C4 + C1)**2 + x1*D*(y31*z41 - y41*z31)*(d21**2 - C2 + C1)
@@ -83,12 +64,12 @@ class TDOA(object):
             c += -1/2*(x31*y21 - x21*y31)*(x31*y41 - x41*y31)*(d21**2 - C2 + C1)*(d41**2 - C4 + C1)
             c += 1/2*(x21*y41 - x41*y21)*(x31*y21 - x21*y31)*(d31**2 - C3 + C1)*(d41**2 - C4 + C1)
             d1 = (-b + sqrt(b**2 - 4*a*c))/(2*a)
-            M = matrix(((x21, y21, z21),
-                           (x31, y31, z31),
-                           (x41, y41, z41)))
+            M = matrix([[x21, y21, z21],
+                        [x31, y31, z31],
+                        [x41, y41, z41]])
             H = self.getH(r0, r1, r2, r3, r4)
-            x, y, z = -M.I*(matrix(((d21), (d31), (d41))).transpose()*d1 + H)
-        return matrix([[asscalar(x)], [asscalar(y)], [asscalar(z)]])
+            x, y, z = -M.I*(matrix([[d21], [d31], [d41]])*d1 + H)
+        return matrix([[x[0,0]], [y[0,0]], [z[0,0]]])
 
     def getdt(self, r0, r1, r2):
         d1 = self.getDistance(r0, r1)
@@ -114,15 +95,15 @@ class TDOA(object):
         C2 = self.getC(r2)
         C3 = self.getC(r3)
         if (r4 is None):
-            H = 1/2*matrix(((d21**2 - (C2 - C1)),
-                               (d31**2 - (C3 - C1))))
+            H = 1/2*matrix([[d21**2 - (C2 - C1)],
+                            [d31**2 - (C3 - C1)]])
         else:
             d41 = self.c*self.getdt(r0, r1, r4)
             C4 = self.getC(r4)
-            H = 1/2*matrix(((d21**2 - (C2 - C1)),
-                               (d31**2 - (C3 - C1)),
-                               (d41**2 - (C4 - C1))))
-        return H.transpose()
+            H = 1/2*matrix([[d21**2 - (C2 - C1)],
+                            [d31**2 - (C3 - C1)],
+                            [d41**2 - (C4 - C1)]])
+        return H
 
     def getG(self, r0, r1, r2, r3):
         d21 = self.c*self.getdt(r0, r1, r2)
