4896040bc75ba634320681b170028a63d1974027
matvidal <matias.vidal.v@gmail.com>
2019-03-12 17:34:04 +0900

    Added the copyright notice.

--- dpl.py
+++ dpl.py
@@ -1,3 +1,25 @@
+'''
+                                Pypredict
+    Orbit prediction software. Displays the satellites' position and
+    orbital parameters in real time. Simulates satellite localization
+    and deployment.
+    
+    Copyright (C) 2018-2019, Matías Vidal Valladares, matvidal.
+    Authors: Matías Vidal Valladares <matias.vidal.v@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+'''
 from calcOrbitParam import Calc
 from pyorbital import tlefile
 from sat import Sat
