9d3cafc73d3fe1a0e9642e3756a9a9011b968452
matvidal <matias.vidal.v@gmail.com>
2019-03-25 19:22:28 -0300

    Deleted the python version specification while using pip, so that it uses the computer's python3 version.

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ sudo apt-get install libjs-proj4
 sudo apt-get install libproj-dev
 #sudo apt-get install libgeos-dev
 sudo apt-get install python3-pip
-sudo -H pip3.6 install --upgrade pip
-sudo -H pip3.6 install --upgrade pyorbital
-sudo -H pip3.6 install matplotlib==2.2.4
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install --upgrade pyorbital
+sudo -H pip3 install matplotlib==2.2.4
 #sudo -H pip3 install --upgrade matplotlib
-sudo -H pip3.6 install --upgrade scipy
-sudo -H pip3.6 install --upgrade cython
-sudo -H pip3.6 install --upgrade numpy
-sudo -H pip3.6 install --upgrade geos
-sudo -H pip3.6 install --upgrade shapely
-sudo -H pip3.6 install --upgrade pyshp
-sudo -H pip3.6 install --upgrade six
-sudo -H pip3.6 install --upgrade Pillow
-sudo -H pip3.6 install --upgrade Fiona
-sudo -H pip3.6 install --upgrade pykdtree
-sudo -H pip3.6 install --upgrade cartopy
+sudo -H pip3 install --upgrade scipy
+sudo -H pip3 install --upgrade cython
+sudo -H pip3 install --upgrade numpy
+sudo -H pip3 install --upgrade geos
+sudo -H pip3 install --upgrade shapely
+sudo -H pip3 install --upgrade pyshp
+sudo -H pip3 install --upgrade six
+sudo -H pip3 install --upgrade Pillow
+sudo -H pip3 install --upgrade Fiona
+sudo -H pip3 install --upgrade pykdtree
+sudo -H pip3 install --upgrade cartopy
 #sudo apt-get install libtcmalloc-minimal4
 #export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4.3.0"
 
