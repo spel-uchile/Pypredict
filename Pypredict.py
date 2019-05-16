6d19602caa880c71c1a17fb740f85c2571288c45
matvidal <matias.vidal.v@gmail.com>
2019-05-16 17:59:43 -0400

    Minor changes in documentation.

--- Pypredict.py
+++ Pypredict.py
@@ -1,4 +1,4 @@
-'''
+"""
                                 Pypredict
     Orbit prediction software. Displays the satellites' position and
     orbital parameters in real time. Simulates satellite localization
@@ -19,7 +19,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
 from pyorbital import tlefile
 from sat import Sat
 from gui import GUI
