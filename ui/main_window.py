8b51bc266768629575f22d57cd4146c98baef3ba
matvidal <matias.vidal.v@gmail.com>
2019-07-17 20:30:06 -0400

    Improved layout.

--- ui/main_window.py
+++ ui/main_window.py
@@ -294,7 +294,7 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuHelp.menuAction())
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
         self.actionExit.triggered.connect(MainWindow.close)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
