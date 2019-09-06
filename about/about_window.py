1a267c6fb5631a071ac2774e7a79b7566f2753eb
matvidal <matias.vidal.v@gmail.com>
2019-09-06 18:46:17 -0400

    Added an About dialog.

---
+++ about/about_window.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'help_window.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_About(object):
+    def setupUi(self, About):
+        About.setObjectName("About")
+        About.resize(500, 147)
+        About.setMinimumSize(QtCore.QSize(500, 0))
+        self.verticalLayout = QtWidgets.QVBoxLayout(About)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.icon = QtWidgets.QLabel(About)
+        self.icon.setText("")
+        self.icon.setAlignment(QtCore.Qt.AlignCenter)
+        self.icon.setObjectName("icon")
+        self.verticalLayout.addWidget(self.icon)
+        self.version = QtWidgets.QLabel(About)
+        font = QtGui.QFont()
+        font.setBold(True)
+        font.setWeight(75)
+        self.version.setFont(font)
+        self.version.setAlignment(QtCore.Qt.AlignCenter)
+        self.version.setObjectName("version")
+        self.verticalLayout.addWidget(self.version)
+        self.textEdit = QtWidgets.QTextEdit(About)
+        self.textEdit.setMinimumSize(QtCore.QSize(450, 83))
+        self.textEdit.setReadOnly(True)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+
+        self.retranslateUi(About)
+        QtCore.QMetaObject.connectSlotsByName(About)
+
+    def retranslateUi(self, About):
+        _translate = QtCore.QCoreApplication.translate
+        About.setWindowTitle(_translate("About", "Dialog"))
+        self.version.setText(_translate("About", "Pypredict 2.2.0"))
+        self.textEdit.setHtml(_translate("About", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Copyright (C) 2018-2019, Matías Vidal Valladares.</p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">E-mail: matias.vidal.v@gmail.com</p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This program comes with ABSOLUTELY NO WARRANTY.</p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">See the GNU General Public License, version 3 or later for details.</p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    About = QtWidgets.QDialog()
+    ui = Ui_About()
+    ui.setupUi(About)
+    About.show()
+    sys.exit(app.exec_())
