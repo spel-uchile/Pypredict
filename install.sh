8a9ddf512dfb01c4fd2e3d48ca32d38c81502c0a
matvidal <matias.vidal.v@gmail.com>
2020-04-18 21:30:26 -0400

    Updated the libraries used. Fixed some bugs. Removed sudo for pip packages installation.

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-sudo apt-get install python3.6-dev
-sudo -H pip3 install --upgrade PyQt5
+sudo apt-get install python3.7-dev
 #sudo apt-get install python3-pyorbital
 #sudo apt-get install python3-matplotlib
 #sudo apt-get install python3-scipy
@@ -10,25 +9,25 @@ sudo apt-get install libxml2-dev libxslt-dev
 sudo apt-get install python3-cairocffi
 sudo apt-get install libatlas-base-dev
 sudo apt-get install libgeos-dev
-sudo apt-get install python3-pip
 sudo apt-get install mongodb
-sudo -H pip3 install --upgrade pip
-sudo -H pip3 install --upgrade pyorbital
-sudo -H pip3 install matplotlib==2.2.4
-#sudo -H pip3 install --upgrade matplotlib
-sudo -H pip3 install --upgrade scipy
-sudo -H pip3 install --upgrade cython
-sudo -H pip3 install --upgrade numpy
-sudo -H pip3 install --upgrade geos
-sudo -H pip3 install --upgrade shapely
-sudo -H pip3 install --upgrade pyshp
-sudo -H pip3 install --upgrade six
-sudo -H pip3 install --upgrade Pillow
-sudo -H pip3 install --upgrade Fiona
-sudo -H pip3 install --upgrade pykdtree
-sudo -H pip3 install --upgrade cartopy
-sudo -H pip3 install --upgrade sgp4
-sudo -H pip3 install --upgrade pymongo
+sudo apt-get install python3-pip
+pip3 install --upgrade pip
+pip3 install --upgrade PyQt5
+pip3 install --upgrade pyorbital
+pip3 install --upgrade matplotlib
+pip3 install --upgrade scipy
+pip3 install --upgrade cython
+pip3 install --upgrade numpy
+pip3 install --upgrade geos
+pip3 install --upgrade shapely --no-binary shapely
+pip3 install --upgrade pyshp
+pip3 install --upgrade six
+pip3 install --upgrade Pillow
+pip3 install --upgrade Fiona
+pip3 install --upgrade pykdtree
+pip3 install --upgrade cartopy
+pip3 install --upgrade sgp4
+pip3 install --upgrade pymongo
 #sudo apt-get install libtcmalloc-minimal4
 #export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4.3.0"
 
