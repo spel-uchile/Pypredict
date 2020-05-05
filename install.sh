d7ecd3262e814997338108ea4188391e88ccec6c
matvidal <matias.vidal.v@gmail.com>
2020-05-04 20:51:21 -0400

    Removed the pip installations and left only the dependencies.

--- install.sh
+++ install.sh
@@ -8,20 +8,3 @@ sudo apt-get install libatlas-base-dev
 sudo apt-get install libgeos-dev
 sudo apt-get install mongodb
 sudo apt-get install python3-pip
-pip3 install --upgrade pip
-pip3 install --upgrade PyQt5
-pip3 install --upgrade pyorbital
-pip3 install --upgrade matplotlib
-pip3 install --upgrade scipy
-pip3 install --upgrade cython
-pip3 install --upgrade numpy
-pip3 install --upgrade geos
-pip3 install --upgrade shapely --no-binary shapely
-pip3 install --upgrade pyshp
-pip3 install --upgrade six
-pip3 install --upgrade Pillow
-pip3 install --upgrade Fiona
-pip3 install --upgrade pykdtree
-pip3 install --upgrade cartopy
-pip3 install --upgrade sgp4
-pip3 install --upgrade pymongo
