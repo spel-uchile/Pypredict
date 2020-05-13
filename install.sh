2d1796a709e1454973d72aef5832a83b2050ef0a
matvidal <matias.vidal.v@gmail.com>
2020-05-13 14:32:20 -0400

    Ordered the installation alphabetically.

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
-sudo apt-get -y install python3-dev
-sudo apt-get -y install proj-bin
-sudo apt-get -y install libjs-proj4
+sudo apt-get -y install libgeos-dev
 sudo apt-get -y install libproj-dev
 sudo apt-get -y install libxml2-dev libxslt-dev
-sudo apt-get -y install libgeos-dev
 sudo apt-get -y install mongodb
+sudo apt-get -y install proj-bin
+sudo apt-get -y install python3-dev
 sudo apt-get -y install python3-pip
